50d80a63b1fc26d669f7c87474cf8325e5984105
AlexDoss42 <alex.doss42@gmail.com>
2019-07-11 13:41:33 -0600

    Having trouble connecting to DB see nodemon for error

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const massive = require('massive')
 
 const app = express();
 
+const { CONNECTION_STRING } = process.env
+
 app.use(express.json());
+
+massive(CONNECTION_STRING)
+  .then((db) => {
+    app.set('db', db)
+    console.log(`It's working! IT'S WORKING!!!!`);
+  })
+  .catch((err) => {
+    console.log('you have an err connecting to your database')
+    console.log(err)
+  })
 
 const PORT = process.env.SERVER_PORT || 4000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
